d5124be025265812fbc7a142e596af181545f46b
FieBee <phuonglf98@gmail.com>
2022-09-15 09:52:58 +0700

    ok

---
+++ src/main/java/com/example/thusermanagement/repository/imp/ICustomerRepository.java
@@ -0,0 +1,7 @@
+package com.example.thusermanagement.repository.imp;
+
+import com.example.thusermanagement.model.Customer;
+import com.example.thusermanagement.repository.IGeneralRepository;
+
+public interface ICustomerRepository extends IGeneralRepository<Customer> {
+}
